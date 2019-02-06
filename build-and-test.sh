f034bb6bedeaef8ad4eb06a33edebbfc42a5bc6d
dknb0709 <ibot07092@gmail.com>
2019-02-06 20:02:36 +0900

    build run-test show-coverage

--- build-and-test.sh
+++ build-and-test.sh
@@ -1,9 +1,19 @@
-#!bin/bash
+#!/bin/bash
 
 ROOT=`pwd`
 
-BIN_DIR="$ROOT/build/test"
+BIN_DIR="$ROOT/build"
 INC_DIR="$ROOT/include"
+
+function build()
+{
+  echo build start [$ROOT]
+  rm -rf $BIN_DIR
+  mkdir $BIN_DIR && cd $BIN_DIR
+  cmake .. -DBUILD_TYPE=coverage
+  make
+  echo build finish
+}
 
 function run-tests()
 {
@@ -13,5 +23,18 @@ function run-tests()
   done
 }
 
+function show_coverage()
+{
+  cd $BIN_DIR/test
+  gcov ../../src/*.cpp --object-directory ../src/CMakeFiles/travis-test-project.dir/
+  find . -name "*.cpp.gcov" -print0 | while read -d $'\0' f
+  do
+    cat "$f"
+  done
+}
+
+build
+
 run-tests $BIN_DIR test.out
 
+show_coverage
